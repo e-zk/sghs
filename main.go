bc0f5379754e54f5a6793926ca1efe082a54a306
e-zk <e-zk@users.noreply.github.com>
2022-02-23 17:42:52 +1000

    Add check for cert+key files
    - improve comments
    - improve usage strings

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 	"strings"
 )
 
@@ -11,26 +12,36 @@ func main() {
 	var addr, path, tls, certFile, keyFile string
 	flag.StringVar(&addr, "l", ":8080", "[address]:port to listen on")
 	flag.StringVar(&path, "p", ".", "path to serve")
-	flag.StringVar(&tls, "t", "", "tls cert_path:key_path")
+	flag.StringVar(&tls, "t", "", "tls cert and key path in the format of: 'cert_path:key_path'")
 	flag.Parse()
 
+	// if tls cert has been given, we need to check if the files exist
 	if tls != "" {
 		s := strings.Split(tls, ":")
 		if len(s) < 2 || s[0] == "" || s[1] == "" {
 			log.Fatal("Invalid cert/key path.")
 		}
 
-		// TODO check if files exist
 		certFile = s[0]
+		_, err := os.Open(certFile)
+		if os.IsNotExist(err) {
+			log.Fatal("Given cert file does not exist.")
+		}
 		keyFile = s[1]
+		_, err = os.Open(keyFile)
+		if os.IsNotExist(err) {
+			log.Fatal("Given cert key file does not exist.")
+		}
 	}
 
 	// TODO custom handler that logs requests
 	// TODO -q (quiet) flag (no logging)
 	http.Handle("/", http.FileServer(http.Dir("./")))
 
+	// if tls cert has been given, serve tls
 	if tls != "" {
-		log.Printf("Serving %s on %s (tls=%s:%s).\n", path, addr, certFile, keyFile)
+		log.Printf("Serving %s on %s (tls).\n", path, addr)
+		log.Printf("Using:\ncert: %s\nkey: %s\n", certFile, keyFile)
 		if err := http.ListenAndServeTLS(addr, certFile, keyFile, nil); err != nil {
 			log.Fatal(err)
 		}
