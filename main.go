e76c005513657df9242c9cede74e3599b1e48e51
e-zk <e-zk@users.noreply.github.com>
2022-02-23 17:28:22 +1000

    Add initial TLS support
    - add support for serving TLS
    - add '-t' tls flag to supply certs
    - rename flag '-a' to '-l'
    - improve usage strings

--- main.go
+++ main.go
@@ -4,17 +4,40 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"strings"
 )
 
 func main() {
-	var addr, path string
-	flag.StringVar(&addr, "a", ":8080", "address:port to serve from")
-	flag.StringVar(&path, "p", ".", "directory to serve")
+	var addr, path, tls, certFile, keyFile string
+	flag.StringVar(&addr, "l", ":8080", "[address]:port to listen on")
+	flag.StringVar(&path, "p", ".", "path to serve")
+	flag.StringVar(&certFile, "t", "", "tls cert_path:key_path")
 	flag.Parse()
 
-	log.Printf("Serving %s on %s\n", path, addr)
+	if tls != "" {
+		s := strings.Split(tls, ":")
+		if len(s) < 2 || s[0] == "" || s[1] == "" {
+			log.Fatal("Invalid cert/key path.")
+		}
+
+		// TODO check if files exist
+		certFile = s[0]
+		keyFile = s[1]
+	}
+
+	// TODO custom handler that logs requests
+	// TODO -q (quiet) flag (no logging)
 	http.Handle("/", http.FileServer(http.Dir("./")))
-	if err := http.ListenAndServe(addr, nil); err != nil {
-		log.Fatal(err)
+
+	if tls != "" {
+		log.Printf("Serving %s on %s (tls=%s:%s).\n", path, addr, certFile, keyFile)
+		if err := http.ListenAndServeTLS(addr, certFile, keyFile, nil); err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		log.Printf("Serving %s on %s.\n", path, addr)
+		if err := http.ListenAndServe(addr, nil); err != nil {
+			log.Fatal(err)
+		}
 	}
 }
